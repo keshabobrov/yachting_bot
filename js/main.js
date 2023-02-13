a94775469d8154b80c7181f20949a30d34325a2e
vikentybobrov <keshabobrov@gmail.com>
2023-02-13 19:45:26 +0300

    7 push

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 function GetUserData() {
-    alert('started 6')
-    const data = Telegram.WebApp.initDataUnsafe
-    Telegram.WebApp.showAlert(data.user.id);
+    let tg = window.Telegram.WebApp;
+    alert('started 7');
+    const data = tg.initDataUnsafe.user.id
+    Telegram.WebApp.showAlert(data);
     alert("pressed!")
 }
